80ae860b33784d9f5c1f11d2f27464686bc3aa05
dedan2640 <dedanireri2640@gmail.com>
2025-02-11 05:41:58 +0000

    on variables

---
+++ variable.js
@@ -0,0 +1,10 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavascript
+/* Comments can make code readable,
+easy to reuse
+and informative*/
+
+var x = "this is a string";
+var student = true;
+var u = undefined;
+var z = null;
