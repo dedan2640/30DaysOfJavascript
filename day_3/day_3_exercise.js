c2b26468a413dd2f3fffcbd0651e8f2ac0056304
dedan2640 <dedanireri2640@gmail.com>
2025-02-17 22:17:59 +0300

    day three

---
+++ day_3/day_3_exercise.js
@@ -0,0 +1,74 @@
+
+//variables
+var firstName = "Dedan";
+var lastName = "Ireri";
+var country = "Kenya";
+var city = "Nairobi";
+var isMarried = true;
+var year = "2025";
+
+//checking datatypes
+typeof(firstName)
+typeof(lastName)
+typeof(country)
+typeof(isMarried)
+typeof(year)
+
+//determine if two variables are of same datatype
+var number10 = 10;
+var string ="10";
+if (number = string) {
+    console.log("equal")
+}else{
+    console.log("not equal")
+}
+
+var float =parseInt(9.8)
+var number10 = 10;
+var string ="10";
+
+//determine if two variables are of same datatype
+if (number10 = float) {
+    console.log("equal")
+}else{
+    console.log("not equal")
+}
+
+//three statements set to truth value
+var student = true
+var philadelphian = true
+var km_settler = true
+
+//three statements set to false value
+var finalist = false
+var issacharite = false
+var lecturer = false
+
+//confirm the result values
+console.log(4 > 3)
+console.log(4 >= 3)
+console.log(4 < 3)
+console.log(4 <= 3)
+console.log(4 == 4)
+console.log(4 === 4)
+console.log(4 != 4)
+console.log(4 !== 4)
+console.log(4 != '4')
+console.log(4 == '4')
+console.log(4 === '4')
+
+length("python")
+length("jargon")
+
+
+
+console.log(4 > 3 && 10 < 12)
+console.log(4 > 3 && 10 > 12)
+console.log(4 > 3 || 10 < 12)
+console.log(4 > 3 || 10 > 12)
+console.log((!4 > 3))
+console.log((!4 < 3))
+console.log(!(false))
+console.log(!(4 > 3 && 10 < 12))
+console.log(!(4 > 3 && 10 > 12))
+console.log(!(4 === '4'))
